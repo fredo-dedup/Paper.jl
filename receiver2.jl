576151c75dafb49d6261448dd4e31044bdcba947
fredo <frederic.testard@gmail.com>
2015-08-28 19:37:38 +0200

    new tests

--- receiver2.jl
+++ receiver2.jl
@@ -1,26 +1,24 @@
 
-news2 = Input("")
-lines = AbstractString["starting..."]
+function main(window)
+    news3 = Input("")
+    lines = AbstractString["starting..."]
 
-@async begin
-    server = listen(2016)
-    while true
-        sock = accept(server)
-        println("accepted !")
-        @async while isopen(sock)
-            println("waiting for line")
-            ns = readline(sock)
-            println("line found! : $ns")
-            @async push!(news2, ns)
-            println(lines)
+    @async begin
+        server = listen(2016)
+        while true
+            sock = accept(server)
+            println("accepted !")
+            @async while isopen(sock)
+                println("waiting for line")
+                ns = readline(sock)
+                println("line found! : $ns")
+                @async push!(news3, ns)
+                println(lines)
+            end
         end
     end
-end
 
-function main(window)
-
-
-    lift(news2) do ns
+    lift(news3) do ns
         push!(lines, ns)
         vbox(lines...) |> pad(2em)
     end
